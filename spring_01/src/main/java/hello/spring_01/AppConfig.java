71029793fc07bb87c9df47270144cc981b8c75a8
hoonloper <yonghoonj98@gmail.com>
2024-05-01 16:51:48 +0900

    Refactor: AppConfig 리팩토링

--- spring_01/src/main/java/hello/spring_01/AppConfig.java
+++ spring_01/src/main/java/hello/spring_01/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.spring_01;
 
+import hello.spring_01.discount.DiscountPolicy;
 import hello.spring_01.discount.FixDiscountPolicy;
+import hello.spring_01.member.MemberRepository;
 import hello.spring_01.member.MemberService;
 import hello.spring_01.member.MemberServiceImpl;
 import hello.spring_01.member.MemoryMemberRepository;
@@ -9,10 +11,18 @@ import hello.spring_01.order.OrderServiceImpl;
 
 public class AppConfig {
   public MemberService memberService() {
-    return new MemberServiceImpl(new MemoryMemberRepository());
+    return new MemberServiceImpl(memberRepository());
   }
 
   public OrderService orderService() {
-    return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    return new OrderServiceImpl(memberRepository(), discountPolicy());
+  }
+
+  public MemberRepository memberRepository() {
+    return new MemoryMemberRepository();
+  }
+
+  public DiscountPolicy discountPolicy() {
+    return new FixDiscountPolicy();
   }
 }
