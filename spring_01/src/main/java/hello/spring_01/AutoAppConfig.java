ba2309bfe74e9c7745aaf7f16eab396b9dbc8020
hoonloper <yonghoonj98@gmail.com>
2024-05-05 14:43:26 +0900

    Feat: 탐색 위치와 기본 스캔 대상

--- spring_01/src/main/java/hello/spring_01/AutoAppConfig.java
+++ spring_01/src/main/java/hello/spring_01/AutoAppConfig.java
@@ -6,6 +6,10 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+  // default package는 이 class가 존재하는 패키지의 하위 모든 Component를 스캔한다.
+  // 즉 패키지 위치를 지정하지 않고, 설정 정보 클래스의 위치를 프로젝트 최상단에 두는 것, 최근 스프링 부트도 이 방법을 기본으로 제공
+  basePackages = "hello.spring_01.member",
+  basePackageClasses = AutoAppConfig.class,
   excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
