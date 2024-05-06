70cb7e1e9c6b64385921c8f9d9078a74ba7995d7
hoonloper <yonghoonj98@gmail.com>
2024-05-06 14:30:57 +0900

    Feat: @Autowired 필드 명, @Qualifier, @Primary

--- spring_01/src/main/java/hello/spring_01/discount/FixDiscountPolicy.java
+++ spring_01/src/main/java/hello/spring_01/discount/FixDiscountPolicy.java
@@ -2,9 +2,11 @@ package hello.spring_01.discount;
 
 import hello.spring_01.member.Grade;
 import hello.spring_01.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy {
   private int discountFixAmount = 1_000;
 
--- spring_01/src/main/java/hello/spring_01/order/OrderServiceImpl.java
+++ spring_01/src/main/java/hello/spring_01/order/OrderServiceImpl.java
@@ -5,6 +5,7 @@ import hello.spring_01.member.Member;
 import hello.spring_01.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,8 +14,32 @@ public class OrderServiceImpl implements OrderService {
   private final MemberRepository memberRepository;
   private final DiscountPolicy discountPolicy;
 
+  /**
+   * @Autowired 매칭 정리
+   * 1. 타입 매칭
+   * 2. 타입 매칭의 결과가 2개 이상일 때 필드명, 파라미터명으로 빈 이름 매칭
+   *
+   * @Qualifier
+   * 추가 구분자를 붙여주는 방법
+   * 주입 시 추가적인 방법을 제공하는 것이지 빈 이름을 변경하는 것은 아니다
+   * Qualifier는 Qualifier를 찾는 용도로만 사용하는 게 좋다
+   *
+   * @Primary
+   * 우선순위를 정하는 방법이다. @Autowired 시에 여러 빈이 조회되면 @Primary가 우선권을 가진다
+   *
+   * # @Qualifier, @Primary
+   * 코드에서 자주 사용하는 메인 데이터베이스의 커넥션을 획득하는 스프링 빈이 있고,
+   * 코드에서 특별한 기능으로 가끔 사용하는 서브 데이터베이스의 커넥션을 획득하는 스프링 빈이 있다고 생각해보자
+   * 메인 데이터베이스의 커넥션을 획득하는 스프링 빈은 @Primary를 적용해서 조회하는 곳에서 @Qualifier 지정 없이 편리하게 조회하고,
+   * 서브 데이터베이스 커넥션 빈을 획득할 때는 @Qualifier를 지정해서 명시적으로 획득하는 방식으로 사용하면 코드를 깔끔하게 유지할 수 있다
+   * 물론 이때 메인 데이터베이스의 스프링 빈을 등록할 때 @Qualifier를 지정해주는 것은 상관없다
+   * [우선순위]
+   * -> @Primary는 기본값 처럼 동작하는 것이고, @Quailifier는 매우 상세하게 동작한다.
+   * 이런 경우 어떤 것이 우선권을 가져갈까? 스프링은 자동보다는 수동이, 넓은 범위의 선택권 보다는 좁은 범위의 선택권이 우선 순위가 높다.
+   * 따라서 여기서도 @Quailifier가 우선권이 높다
+   */
   @Autowired
-  public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+  public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
     this.memberRepository = memberRepository;
     this.discountPolicy = discountPolicy;
   }
