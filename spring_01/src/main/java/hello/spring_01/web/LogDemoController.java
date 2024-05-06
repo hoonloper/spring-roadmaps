93ee5233b04b189279a1bd02e4c842ede2657f93
hoonloper <yonghoonj98@gmail.com>
2024-05-06 20:00:15 +0900

    Feat: 스코프와 Provider

--- spring_01/src/main/java/hello/spring_01/web/LogDemoController.java
+++ spring_01/src/main/java/hello/spring_01/web/LogDemoController.java
@@ -3,6 +3,7 @@ package hello.spring_01.web;
 import hello.spring_01.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,17 +12,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class LogDemoController {
   private final LogDemoService logDemoService;
-  private final MyLogger myLogger;
+  private final ObjectProvider<MyLogger> myLoggerProvider;
 
   @RequestMapping("log-demo")
   @ResponseBody
   public String logDemo(HttpServletRequest request) {
     String requestURL = request.getRequestURL().toString();
-    myLogger.setRequestURL(requestURL);
 
+    MyLogger myLogger = myLoggerProvider.getObject();
+    myLogger.setRequestURL(requestURL);
     myLogger.log("controller test");
+
     logDemoService.logic("testId");
 
-    return "test";
+    return "OK";
   }
 }
