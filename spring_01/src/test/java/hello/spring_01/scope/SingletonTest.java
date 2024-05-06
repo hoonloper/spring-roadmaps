463bae249cab79266e88ae8cc1db70d97adfc12a
hoonloper <yonghoonj98@gmail.com>
2024-05-06 16:59:29 +0900

    Feat: 프로토타입 스코프

---
+++ spring_01/src/test/java/hello/spring_01/scope/SingletonTest.java
@@ -0,0 +1,46 @@
+package hello.spring_01.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class SingletonTest {
+  @Test
+  void singletonBeanFind() {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+
+    SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+    SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+
+    System.out.println("singletonBean1 = " + singletonBean1);
+    System.out.println("singletonBean2 = " + singletonBean2);
+
+    Assertions.assertThat(singletonBean1).isSameAs(singletonBean2);
+
+    ac.close();
+  }
+
+  /**
+   * 싱글톤 빈은 스프링 컨테이너 생성 시점에 초기화 메서드가 실행 되지만, 프로토타입 스코프의 빈은 스프링 컨테 이너에서 빈을 조회할 때 생성되고, 초기화 메서드도 실행된다.
+   * 프로토타입 빈을 2번 조회했으므로 완전히 다른 스프링 빈이 생성되고, 초기화도 2번 실행된 것을 확인할 수 있다.
+   * 싱글톤 빈은 스프링 컨테이너가 관리하기 때문에 스프링 컨테이너가 종료될 때 빈의 종료 메서드가 실행되지만,
+   * 프로토타입 빈은 스프링 컨테이너가 생성과 의존관계 주입 그리고 초기화 까지만 관여하고, 더는 관리하지 않는다.
+   * 따라서 프로토타입 빈은 스프링 컨테이너가 종료될 때 `@PreDestroy` 같은 종료 메서드가 전혀 실행되지 않는다.
+   */
+  @Scope("singleton")
+  static class SingletonBean {
+    @PostConstruct
+    public void init() {
+      System.out.println("SingletonBean.init");
+    }
+
+    @PreDestroy
+    public void destroy() {
+      System.out.println("SingletonBean.destroy");
+    }
+  }
+}
