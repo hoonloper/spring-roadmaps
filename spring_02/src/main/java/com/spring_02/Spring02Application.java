e2abc8c27185814fccb134743a0fdf488e9f9ffd
hoonloper <yonghoonj98@gmail.com>
2024-05-14 20:55:15 +0900

    Feat: Hello 서블릿

--- spring_02/src/main/java/com/spring_02/Spring02Application.java
+++ spring_02/src/main/java/com/spring_02/Spring02Application.java
@@ -2,7 +2,9 @@ package com.spring_02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class Spring02Application {
 
