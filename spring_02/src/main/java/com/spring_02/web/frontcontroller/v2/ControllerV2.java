4293ba758746082c2ee466cf0bede12b943d922f
hoonloper <yonghoonj98@gmail.com>
2024-05-19 16:15:42 +0900

    Feat: View 분리 - v2

---
+++ spring_02/src/main/java/com/spring_02/web/frontcontroller/v2/ControllerV2.java
@@ -0,0 +1,12 @@
+package com.spring_02.web.frontcontroller.v2;
+
+import com.spring_02.web.frontcontroller.MyView;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public interface ControllerV2 {
+  MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+}
