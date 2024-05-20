bf9b0e0c89ec19b9c0ed32a2bd63744dfb5892d5
hoonloper <yonghoonj98@gmail.com>
2024-05-20 21:49:43 +0900

    Feat: 뷰 리졸버

--- spring_02/src/main/java/com/spring_02/Spring02Application.java
+++ spring_02/src/main/java/com/spring_02/Spring02Application.java
@@ -3,6 +3,9 @@ package com.spring_02;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan
 @SpringBootApplication
@@ -12,4 +15,9 @@ public class Spring02Application {
     SpringApplication.run(Spring02Application.class, args);
   }
 
+// 스프링 부트가 다 해줌
+//  @Bean
+//  ViewResolver internalResourceViewResolver() {
+//    return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+//  }
 }
