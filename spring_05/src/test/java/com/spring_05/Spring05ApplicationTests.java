650e735fa76e0fde0cc66e5a8ae1e478c9c0cc97
hoonloper <yonghoonj98@gmail.com>
2024-06-10 20:50:46 +0900

    Setting: 프로젝트 세팅

---
+++ spring_05/src/test/java/com/spring_05/Spring05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring05ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
