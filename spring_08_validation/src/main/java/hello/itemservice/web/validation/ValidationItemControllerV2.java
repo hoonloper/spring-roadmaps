899b0d0457d7d64d99f0bae6ca2966c8e88214ed
hoonloper <yonghoonj98@gmail.com>
2024-06-16 15:19:24 +0900

    Feat: 오류 코드와 메시지 처리2

--- spring_08_validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ spring_08_validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -115,7 +115,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         // 검증 로직
         if (!StringUtils.hasText(item.getItemName())) {
@@ -141,6 +141,45 @@ public class ValidationItemControllerV2 {
         // 검증에 실패하면 다시 입력 폼으로
         if (bindingResult.hasFieldErrors()) {
             log.info("bindingResult = {}", bindingResult);
+
+            return "validation/v2/addForm";
+        }
+
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        // 검증 로직
+        log.info("objectName={}", bindingResult.getObjectName());
+        log.info("target={}", bindingResult.getTarget());
+
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindingResult.rejectValue("itemName", "required");
+        }
+
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
+        }
+
+        if (item.getQuantity() == null || item.getQuantity() > 10000) {
+            bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
+        }
+
+        //특정 필드 예외가 아닌 전체 예외
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
 
             return "validation/v2/addForm";
         }
