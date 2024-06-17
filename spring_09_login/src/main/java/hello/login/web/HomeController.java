60b2a22fff7c5409c58e5236dd7fefc0a98e2b93
hoonloper <yonghoonj98@gmail.com>
2024-06-17 21:14:53 +0900

    Feat: 로그인 처리하기 - 직접 만든 세션 적용

--- spring_09_login/src/main/java/hello/login/web/HomeController.java
+++ spring_09_login/src/main/java/hello/login/web/HomeController.java
@@ -8,23 +8,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
   private final MemberRepository memberRepository;
+  private final SessionManager sessionManager;
 
   //    @GetMapping("/")
   public String home() {
     return "home";
   }
 
-  @GetMapping("/")
+//  @GetMapping("/")
   public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
     if (memberId == null) {
       return "home";
@@ -42,17 +41,14 @@ public class HomeController {
     return "loginHome";
   }
 
-  @PostMapping("/logout")
-  public String logout(HttpServletResponse response) {
-    expireCookie(response, "memberId");
-    return "redirect:/";
-  }
+  @GetMapping("/")
+  public String homeLoginV2(HttpServletRequest request, Model model) {
+    // 세션 관리자에 저장된 회원 정보 조회
+    Member member = (Member)sessionManager.getSession(request); if (member == null) {
+      return "home";
+    }
 
-  private void expireCookie(HttpServletResponse response, String cookieName) {
-    Cookie cookie = new Cookie(cookieName, null);
-
-    cookie.setMaxAge(0);
-
-    response.addCookie(cookie);
+    // 로그인
+    model.addAttribute("member", member); return "loginHome";
   }
 }
