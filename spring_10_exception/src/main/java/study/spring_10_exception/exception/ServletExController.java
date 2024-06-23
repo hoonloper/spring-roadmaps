4c43d1afd8d0435bf9e2fece56b378d03a6f1e0f
hoonloper <yonghoonj98@gmail.com>
2024-06-23 16:06:36 +0900

    Feat: 서블릿 예외 처리 - 시작

---
+++ spring_10_exception/src/main/java/study/spring_10_exception/exception/ServletExController.java
@@ -0,0 +1,24 @@
+package study.spring_10_exception.exception;
+
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ServletExController {
+  @GetMapping("/error-ex")
+  public void errorEx() {
+    throw new RuntimeException("예외 발생!");
+  }
+
+  @GetMapping("/error-404")
+  public void error404(HttpServletResponse response) throws IOException {
+    response.sendError(404, "404 오류!");
+  }
+
+  @GetMapping("/error-500")
+  public void error500(HttpServletResponse response) throws IOException {
+    response.sendError(500);
+  }
+}
