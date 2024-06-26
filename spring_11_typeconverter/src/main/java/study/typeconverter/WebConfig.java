c959b3df6fb08997aa9926a7b59e78f4b26c6d13
hoonloper <yonghoonj98@gmail.com>
2024-06-26 21:02:16 +0900

    Feat: Formatter 적용

--- spring_11_typeconverter/src/main/java/study/typeconverter/WebConfig.java
+++ spring_11_typeconverter/src/main/java/study/typeconverter/WebConfig.java
@@ -7,15 +7,19 @@ import study.typeconverter.converter.IntegerToStringConverter;
 import study.typeconverter.converter.IpPortToStringConverter;
 import study.typeconverter.converter.StringToIntegerConverter;
 import study.typeconverter.converter.StringToIpPortConverter;
+import study.typeconverter.formatter.MyNumberFormatter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addFormatters(FormatterRegistry registry) {
-    registry.addConverter(new StringToIntegerConverter());
-    registry.addConverter(new IntegerToStringConverter());
+    // registry.addConverter(new StringToIntegerConverter());
+    // registry.addConverter(new IntegerToStringConverter());
     registry.addConverter(new StringToIpPortConverter());
     registry.addConverter(new IpPortToStringConverter());
+
+    // 추가
+    registry.addFormatter(new MyNumberFormatter());
   }
 }
