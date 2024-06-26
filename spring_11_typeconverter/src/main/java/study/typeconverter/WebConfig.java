e9a32e29d82cfc8ecd8a0ea40f157097bc3de991
hoonloper <yonghoonj98@gmail.com>
2024-06-26 20:54:49 +0900

    Feat: 스프링에 Converter 적용

---
+++ spring_11_typeconverter/src/main/java/study/typeconverter/WebConfig.java
@@ -0,0 +1,21 @@
+package study.typeconverter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import study.typeconverter.converter.IntegerToStringConverter;
+import study.typeconverter.converter.IpPortToStringConverter;
+import study.typeconverter.converter.StringToIntegerConverter;
+import study.typeconverter.converter.StringToIpPortConverter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addFormatters(FormatterRegistry registry) {
+    registry.addConverter(new StringToIntegerConverter());
+    registry.addConverter(new IntegerToStringConverter());
+    registry.addConverter(new StringToIpPortConverter());
+    registry.addConverter(new IpPortToStringConverter());
+  }
+}
