69c3d5c9c904177fbda3df5176472a25bc4b2cad
hoonloper <yonghoonj98@gmail.com>
2024-06-27 20:19:08 +0900

    Setting: upload

---
+++ spring_12_upload/src/main/java/study/upload/Spring11UploadApplication.java
@@ -0,0 +1,13 @@
+package study.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring11UploadApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Spring11UploadApplication.class, args);
+  }
+
+}
