4f50f166bf7bd303c8ee3121615adee7de850020
hoonloper <yonghoonj98@gmail.com>
2024-06-28 20:47:20 +0900

    Feat: 파일 업로드와 다운로드

---
+++ spring_12_upload/src/main/java/study/upload/domain/Item.java
@@ -0,0 +1,12 @@
+package study.upload.domain;
+
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class Item {
+  private Long id;
+  private String itemName;
+  private UploadFile attachFile;
+  private List<UploadFile> imageFiles;
+}
