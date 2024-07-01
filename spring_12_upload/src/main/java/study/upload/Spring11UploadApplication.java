18eaa3efb6c789d0490c8eb5630bcf2651db2696
hoonloper <yonghoonj98@gmail.com>
2024-07-01 21:33:58 +0900

    리팩토링

--- spring_12_upload/src/main/java/study/upload/Spring11UploadApplication.java
+++ spring_12_upload/src/main/java/study/upload/Spring11UploadApplication.java
@@ -9,5 +9,4 @@ public class Spring11UploadApplication {
   public static void main(String[] args) {
     SpringApplication.run(Spring11UploadApplication.class, args);
   }
-
 }
--- spring_12_upload/src/main/java/study/upload/domain/Item.java
+++ spring_12_upload/src/main/java/study/upload/domain/Item.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 @Data
 public class Item {
+
   private Long id;
   private String itemName;
   private UploadFile attachFile;
